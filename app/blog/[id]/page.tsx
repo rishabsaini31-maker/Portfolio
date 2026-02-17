15852ff945e4f473a6570568e11adaa448265698
Rishab Saini <rishbasiniupw165@gmail.com>
2026-02-17 09:35:04 +0530

    Fix ESLint error: escape unescaped quotes in blog post page

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -494,7 +494,7 @@ export default function BlogPostPage() {
               Post Not Found
             </h1>
             <p className="text-gray-400 mb-8">
-              The blog post you're looking for doesn't exist.
+              The blog post you{"'"}re looking for doesn{"'"}t exist.
             </p>
             <Link
               href="/blog"
